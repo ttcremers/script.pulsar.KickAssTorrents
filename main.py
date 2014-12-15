aac1923bdcd2fdfec13798689ff2cd1566241f06
Teitr <ttcremers@gmail.com>
2014-12-15 19:50:57 +0100

    fixed provider to match new site situation of kickasstorrents, also made tv episode searches more reliable and specific

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from pulsar import provider
+# Game of Thrones 720p OR 1080p -complete category:tv seeds:10 lang_id:2 season:2 episode:4
 
 def search(query):
-	resp = provider.GET("http://kickass.to/search", params={
+	resp = provider.GET("http://kickass.so/usearch", params={
 		"q": query,
 	})
 	return provider.extract_magnets(resp.data)
 
 def search_episode(episode):
-	return search("%(title)s S%(season)02dE%(episode)02d" % episode)
+	return search("%(title)s S%(season)02dE%(episode)02d category:tv lang_id:2 -complete 720p OR 1080p" % episode)
 
 def search_movie(movie):
 	return search("%(title)s %(year)d" % movie)
 
-
 provider.register(search, search_movie, search_episode)
-
-
